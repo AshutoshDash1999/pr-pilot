ade47d9381e257b32a59e9763b997e6b761bd704
ashutosh-fixity <ashutosh.d@fixitylearn.com>
2025-08-29 00:46:05 +0530

    feat: integrate Tailwind CSS and DaisyUI for enhanced styling

--- src/ui/App.tsx
+++ src/ui/App.tsx
@@ -1,7 +1,13 @@
 import './App.css';
 
 function App() {
-  return <main className="container">hello world!!!</main>;
+  return (
+    <main className="container">
+      hello world!!!
+      <button className="btn btn-primary">Button</button>
+      <button className="btn w-64 rounded-full">Button</button>
+    </main>
+  );
 }
 
 export default App;
